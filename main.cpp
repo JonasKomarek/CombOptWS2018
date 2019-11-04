248857369162d76978d0ed7d1404ce6809ff356b
Jonas Komarek <j.komarek.98@gmx.de>
2019-11-04 13:37:21 +0100

    Implemented Tarjans algorithm
    Implemented Tarjans alogrithm for strongly connected components in digraphs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "graph.hpp"
 #include "randomgraph.hpp"
 #include <iostream>
+#include <time.h>
 
 // Convenience. If you don't understand just ignore ;)
 using namespace DHBW;
@@ -60,11 +61,125 @@ std::vector<VertexId> random_path(Graph & g, VertexId s, VertexId t)
 	return path;
 }
 
+void visit1(Graph &g, std::vector<bool> &visited, VertexId &vertexCounter, std::vector<VertexId> &numberedVertices, VertexId v_id)
+{
+	Vertex &currVertex = g.vertex(v_id);
+	visited[v_id] = 1;
+	for (EdgeId j = 0; j < currVertex.num_edges(); ++j)
+	{
+		Edge e = g.edge(currVertex.edge(j));
+		// if edge is incoming continue
+		if (currVertex.id() == e.vertex2())
+		{
+			continue;
+		}
+		else
+		{
+			VertexId neighbour_id = e.other_vertex(currVertex.id());
+			if (0 == visited[neighbour_id])
+			{
+				visit1(g, visited, vertexCounter, numberedVertices, neighbour_id);
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	numberedVertices[vertexCounter] = v_id;
+	++vertexCounter;
+}
+
+void visit2(Graph &g, std::vector<bool> &visited, std::vector<VertexId> &connComponent, VertexId v_id)
+{
+	Vertex &currVertex = g.vertex(v_id);
+	visited[v_id] = 1;
+	for (EdgeId j = 0; j < currVertex.num_edges(); ++j)
+	{
+		Edge e = g.edge(currVertex.edge(j));
+		// if edge is outgoing continue
+		if (currVertex.id() == e.vertex1())
+		{
+			continue;
+		}
+		else
+		{
+			VertexId neighbour_id = e.other_vertex(currVertex.id());
+			if (0 == visited[neighbour_id])
+			{
+				visit2(g, visited, connComponent, neighbour_id);
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	// add vertex to the connected component
+	connComponent.push_back(v_id);
+}
+
+void tarjans(Graph &g, bool printToStdIn = false)
+{
+	std::vector<bool> visited(g.num_vertices(), 0);
+	// counter to number vertices in first run
+	VertexId vertexCounter = 0;
+	// to store vertexIds ordered after their number from the first run
+	std::vector<VertexId> numberedVertices(g.num_vertices(), -1);
+	// to store strongly connected components in the second run
+	std::vector<std::vector<VertexId>> connectedComps;
+
+	for (VertexId i = 0; i < g.num_vertices(); ++i)
+	{
+		if (1 != visited[i])
+		{
+			visit1(g, visited, vertexCounter, numberedVertices, i);
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	// second run
+
+	//set back visited vertices
+	std::fill(visited.begin(), visited.end(), false);
+	for (VertexId j = visited.size() - 1; j >= 0 && j < visited.size(); --j)
+	{
+		VertexId v_id = numberedVertices[j];
+		if (1 != visited[v_id])
+		{
+			connectedComps.push_back(std::vector<EdgeId>());
+			visit2(g, visited, connectedComps.back(), v_id);
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	std::cout << connectedComps.size();
+	if (printToStdIn != false)
+	{
+		for (VertexId row = 0; row <= connectedComps.size(); row++)
+		{
+			std::cout << "\n";
+			for (VertexId col = 0; col < connectedComps[row].size(); col++)
+			{
+				std::cout << connectedComps[row][col] << " ";
+			}
+		}
+	}
+}
 
 int main()
-{	
-	// Read graph from input file 
-	Graph g("input/graph1.plain");
-	// Print to console, std::endl is (almost) equivalent to "\n"
-	std::cout << example_function1(g) << std::endl;
+{
+	clock_t start, end;
+	start = clock();
+	// Read graph from input file
+	Graph g("input/digraph8.plain");
+	tarjans(g, false);
+	end = clock();
+	std::cout << "\nTime (in ms): " << end - start;
 }
